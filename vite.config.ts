424ab24375de559d530fb79daaa34b6d70a9e9b1
Aslan Quentin <quentin.aslan@outlook.com>
2024-02-01 02:20:26 -0500

    :wrench: build configuration updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
-  },
+	plugins: [vue()],
+	resolve: {
+		alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
+	},
 });
